4ecc5d227936b5c4fa6f08e11af807898cd38213
Your Name <you@example.com>
2017-07-06 11:18:54 +0800

    add

--- build-ev3dev-image.sh
+++ build-ev3dev-image.sh
@@ -4,12 +4,19 @@ EV3DEVDIR=$(dirname "$0")
 cd ${EV3DEVDIR}
 EV3DEVDIR=$(pwd)
 
+echo "clean output directory..."
 rm -fr $EV3DEVDIR/output/*
 
+echo "build robertalab systemd service..."
 ./build-robertalab-systemd.sh
+
+echo "build brickman..."
 ./build-brickman.sh
+
+echo "build qrencode..."
 ./build-qrencode.sh
 
+echo "build docker image..."
 docker build -t csdiregistry.azurecr.io/ev3dev-jessie-ev3-stem -f ev3dev-jessie-stem.dockerfile .
 
 build_dir=${EV3DEVDIR}/brickstrap
@@ -20,11 +27,16 @@ fi
 mkdir -p $build_dir
 cd $build_dir
 
+echo "create tar file from docker image..."
 brickstrap create-tar csdiregistry.azurecr.io/ev3dev-jessie-ev3-stem ev3dev.tar
+echo "create bootimage from tar..."
 brickstrap create-image ev3dev.tar ev3dev.img
+imageName=ev3dev_`date +%Y_%m_%d_%H:%M:%S`.img
+mv ev3dev.img $imageName
 
+echo "update bootimage to azure storage..."
 azcopy \
     --source $build_dir \
     --destination https://csdistg.blob.core.windows.net/ev3images \
     --dest-key vv2ZKUmod9oqdKBI7oWnMcPcd5HP0S6VIva8U9QQL/s6SLG1i55la6dKV7qLeMiX6FyxIQFTldGeyOeDCE7JsQ== \
-    --include "ev3dev.img"
+    --include "$imageName"
