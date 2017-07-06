c5dca0f2fe692e5e88bd85d47d832a8eb22c9e7a
Your Name <you@example.com>
2017-07-06 11:07:43 +0800

    add

--- build-ev3dev-image.sh
+++ build-ev3dev-image.sh
@@ -10,19 +10,21 @@ rm -fr $EV3DEVDIR/output/*
 ./build-brickman.sh
 ./build-qrencode.sh
 
-docker build -t ev3dev/ev3dev-jessie-ev3-stem -f ev3dev-jessie-stem.dockerfile
-
-if [ -d "${EV3DEVDIR}/brickstrap" ]; then
-    echo "${EV3DEVDIR}/brickstrap exists, deleting..."
-    rm -fr ${EV3DEVDIR}/brickstrap
-fi
+docker build -t csdiregistry.azurecr.io/ev3dev-jessie-ev3-stem -f ev3dev-jessie-stem.dockerfile .
 
 build_dir=${EV3DEVDIR}/brickstrap
+if [ -d "$build_dir" ]; then
+    echo "$build_dir exists, deleting..."
+    rm -fr $build_dir
+fi
 mkdir -p $build_dir
 cd $build_dir
 
-git clone https://github.com/wangtt03/brickstrap.git
-rm -f ev3dev.tar
-rm -f ev3dev.img
-./brickstrap/src/brickstrap.sh create-tar csdiregistry.azurecr.io/ev3dev-jessie-ev3-base-my2 ev3dev.tar
-./brickstrap/src/brickstrap.sh create-image ev3dev.tar ev3dev.img
+brickstrap create-tar csdiregistry.azurecr.io/ev3dev-jessie-ev3-stem ev3dev.tar
+brickstrap create-image ev3dev.tar ev3dev.img
+
+azcopy \
+    --source $build_dir \
+    --destination https://csdistg.blob.core.windows.net/ev3images \
+    --dest-key vv2ZKUmod9oqdKBI7oWnMcPcd5HP0S6VIva8U9QQL/s6SLG1i55la6dKV7qLeMiX6FyxIQFTldGeyOeDCE7JsQ== \
+    --include "ev3dev.img"
--- ev3dev-jessie-stem.dockerfile
+++ ev3dev-jessie-stem.dockerfile
@@ -1,7 +1,5 @@
 FROM ev3dev/ev3dev-jessie-ev3-generic
 
-RUN sudo mkdir -p /data/projects/
-
 RUN sudo apt-get update && \
     sudo apt-get install --only-upgrade python3-ev3dev && \
     sudo apt-get install -y python3-pip && \
@@ -12,8 +10,8 @@ RUN sudo apt-get update && \
     sudo apt-get install -y python3-bluez python3-dbus python3-gi
 
 COPY ./output/robertalab-service/openrobertalab_1.7.2+1.0.0_all.deb /data/projects/robertalab-service/
-COPY ./output/brickman/brickman/usr/local/sbin/brickman /usr/local/sbin/
-COPY ./output/brickman/brickman/usr/local/share/brickman /usr/local/share/brickman
+COPY ./output/brickman/dist/usr/local/sbin/brickman /usr/local/sbin/
+COPY ./output/brickman/dist/usr/local/share/brickman /usr/local/share/brickman
 COPY ./output/libqrencode/qrencode /usr/bin/
 COPY ./output/libqrencode/libqrencode.so.3 /usr/lib/
 
